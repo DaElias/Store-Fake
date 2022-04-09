51113db7401147a0c20558cf45201ca12eeefc7e
DaElias <58747265+DaElias@users.noreply.github.com>
2022-04-09 18:08:43 -0500

    add info localstoragegit add .

--- src/containers/Information.js
+++ src/containers/Information.js
@@ -1,9 +1,75 @@
-import React, { useRef, useContext } from "react";
+import React, { useRef, useContext, useEffect } from "react";
 import AppContex from "../context/AppContex";
 import { Button, Container, Table } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import Footer from "../Components/Footer";
 import Header from "../Components/Header";
+
+const initialState = {
+  name: "",
+  email: "",
+  address: "",
+  apto: "",
+  city: "",
+  counrty: "",
+  statee: "",
+  cp: "",
+  phone: "",
+};
+
+const formularioReducer = (state, acction) => {
+  switch (acction.type) {
+    case "LOCALSTORAGE_UPDATE":
+      return acction.payload; //* cambia todo el estado!!
+    case "name":
+      return {
+        ...state,
+        name: acction.payload,
+      };
+    case "email":
+      return {
+        ...state,
+        email: acction.payload,
+      };
+    case "address":
+      return {
+        ...state,
+        address: acction.payload,
+      };
+    case "apto":
+      return {
+        ...state,
+        apto: acction.payload,
+      };
+    case "city":
+      return {
+        ...state,
+        city: acction.payload,
+      };
+    case "counrty":
+      return {
+        ...state,
+        counrty: acction.payload,
+      };
+    case "state":
+      return {
+        ...state,
+        state: acction.payload,
+      };
+    case "cp":
+      return {
+        ...state,
+        cp: acction.payload,
+      };
+    case "phone":
+      return {
+        ...state,
+        phone: acction.payload,
+      };
+    default:
+      return state;
+  }
+};
 
 const Information = () => {
   const {
@@ -14,19 +80,25 @@ const Information = () => {
   const form = useRef(null);
   let navigate = useNavigate();
 
+  const [state, dispatch] = React.useReducer(formularioReducer, initialState);
+  const handleFormUpdate = (event) => {
+    const type = event.target.name;
+    const payload = event.target.value;
+    dispatch({ type, payload });
+  };
+
+  useEffect(() => {
+    console.log("Inicio el useEffect");
+    if (localStorage.getItem("buyed")) {
+      console.log("El elemento existe!!");
+      const elemento = JSON.parse(localStorage.getItem("buyed"));
+      console.log(elemento);
+      dispatch({ type: "LOCALSTORAGE_UPDATE", payload: elemento });
+    }
+  }, []);
+
   const handleSubmit = (e) => {
     const formData = new FormData(form.current);
-    const buyed = {
-      name: formData.get("name"),
-      email: formData.get("email"),
-      address: formData.get("address"),
-      apto: formData.get("apto"),
-      city: formData.get("city"),
-      counrty: formData.get("counrty"),
-      state: formData.get("state"),
-      cp: formData.get("cp"),
-      phone: formData.get("phone"),
-    };
     if (
       formData.get("name") === "" ||
       formData.get("email") === "" ||
@@ -39,7 +111,19 @@ const Information = () => {
       formData.get("phone") === ""
     )
       return alert("Asegurese de llenar los datos!!");
+    const buyed = {
+      name: formData.get("name"),
+      email: formData.get("email"),
+      address: formData.get("address"),
+      apto: formData.get("apto"),
+      city: formData.get("city"),
+      counrty: formData.get("counrty"),
+      state: formData.get("state"),
+      cp: formData.get("cp"),
+      phone: formData.get("phone"),
+    };
     addToBuyer(buyed);
+    localStorage.setItem("buyed", JSON.stringify(buyed));
     navigate("/checkout/success"); //redirecciona cuando el pago este listo!!
   };
 
@@ -63,29 +147,76 @@ const Information = () => {
                   placeholder="Nombre completo"
                   name="name"
                   required
+                  value={state.name}
+                  onChange={handleFormUpdate}
                 />
-                <input type="text" placeholder="Correo" name="email" required />
+                <input
+                  type="email"
+                  placeholder="Correo"
+                  name="email"
+                  required
+                  value={state.email}
+                  onChange={handleFormUpdate}
+                />
                 <input
                   type="text"
                   placeholder="Direccion"
                   name="address"
                   required
+                  value={state.address}
+                  onChange={handleFormUpdate}
                 />
-                <input type="text" placeholder="Apto" name="apto" required />
-                <input type="text" placeholder="Ciudad" name="city" required />
-                <input type="text" placeholder="Pais" name="counrty" required />
-                <input type="text" placeholder="Estado" name="state" required />
+
                 <input
                   type="text"
+                  placeholder="Apto"
+                  name="apto"
+                  required
+                  value={state.apto}
+                  onChange={handleFormUpdate}
+                />
+
+                <input
+                  type="text"
+                  placeholder="Ciudad"
+                  name="city"
+                  required
+                  value={state.city}
+                  onChange={handleFormUpdate}
+                />
+
+                <input
+                  type="text"
+                  placeholder="Pais"
+                  name="counrty"
+                  required
+                  value={state.counrty}
+                  onChange={handleFormUpdate}
+                />
+
+                <input
+                  type="text"
+                  placeholder="Estado"
+                  name="state"
+                  required
+                  value={state.state}
+                  onChange={handleFormUpdate}
+                />
+                <input
+                  type="number"
                   placeholder="Codigo postal"
                   name="cp"
                   required
+                  value={state.cp}
+                  onChange={handleFormUpdate}
                 />
                 <input
-                  type="text"
+                  type="number"
                   placeholder="Telefono"
                   name="phone"
                   required
+                  value={state.phone}
+                  onChange={handleFormUpdate}
                 />
                 {/* <div className="Information-buttons"> */}
                 <div className="Information-back">
